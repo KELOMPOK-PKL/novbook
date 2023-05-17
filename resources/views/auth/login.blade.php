8c15a75f958bbde3e0ff8a02505e1e411cb9c40d
NdaruVictor <ndaruvictor55@gmail.com>
2023-05-17 13:56:25 +0700

    first commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -67,7 +67,7 @@
                         <input
                             id="remember_me"
                             type="checkbox"
-                            class="text-purple-500 border-gray-300 rounded focus:border-purple-300 focus:ring focus:ring-purple-500 dark:border-gray-600 dark:bg-dark-eval-1 dark:focus:ring-offset-dark-eval-1"
+                            class="text-gray-800 border-gray-300 rounded focus:border-blue-300 focus:ring focus:ring-blue-500 dark:border-gray-600 dark:bg-dark-eval-1 dark:focus:ring-offset-dark-eval-1"
                             name="remember"
                         >
 
