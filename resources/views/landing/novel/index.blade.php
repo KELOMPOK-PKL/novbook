ba1b878e5c61e7eb997548f850c1eaadc41c12fd
RifqiArdani <muhammadrifqiardani6@gmail.com>
2023-06-22 12:04:47 +0700

    padding

--- resources/views/landing/novel/index.blade.php
+++ resources/views/landing/novel/index.blade.php
@@ -4,11 +4,11 @@
         <!-- Section: Design Block -->
         <section class="mb-32">
             <div
-                class="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+                class="h-3/4 rounded-lg bg-white shadow-lg dark:bg-neutral-700">
                 <div class="flex flex-wrap items-center">
-                    <div class=" w-full shrink-0 grow-0 basis-auto lg:flex lg:w-6/12 xl:w-4/12 ">
+                    <div class="w-full shrink-0 grow-0 basis-auto lg:flex lg:w-6/12 xl:w-4/12">
                         <img src="{{ asset('storage/' . $novel->image) }}" alt="..."
-                            class="w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg" />
+                            class="container rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg w-2/3 h-2/3"  />
                     </div>
                     <div class="w-full shrink-0 grow-0 basis-auto lg:w-6/12 xl:w-8/12">
                         <div class="px-6 py-12 md:px-12">
