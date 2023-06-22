b81462d071590d9a872c439dc9c142a420fbc5b0
NdaruVictor <ndaruvictor55@gmail.com>
2023-06-22 11:08:36 +0700

    add :star icon

--- resources/views/landing/novel/index.blade.php
+++ resources/views/landing/novel/index.blade.php
@@ -15,25 +15,73 @@
                             <h2 class="mb-20 text-2xl font-bold">
                                 {{ $novel->title }}
                             </h2>
-                            <p class="mb-6 text-neutral-500 dark:text-neutral-300">
-                                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                                Earum maxime voluptas ipsam aliquam itaque cupiditate
-                                provident architecto expedita harum culpa odit, inventore rem
-                                molestias laborum repudiandae corporis pariatur quo eius iste!
-                                Quaerat, assumenda voluptates! Molestias, recusandae? Maxime
-                                fuga omnis ducimus.
-                            </p>
-                            <div class="grid grid-cols-3 gap-1">
-                                <p class="text-neutral-800 dark:text-neutral-300 mt-10">
-                                    <i class="fa-sharp fa-solid fa-eye"></i>
-                                    {{ $novel->views_count }}
+                            <div>
+                                <p class=" text-neutral-500 dark:text-neutral-300">
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                                    Earum maxime voluptas ipsam aliquam itaque cupiditate
+                                    provident architecto expedita harum culpa odit, inventore rem
+                                    molestias laborum repudiandae corporis pariatur quo eius iste!
+                                    Quaerat, assumenda voluptates! Molestias, recusandae? Maxime
+                                    fuga omnis ducimus.
                                 </p>
-                                <p class="text-neutral-800 dark:text-neutral-300 mt-10 ">
-                                    <i class="fa-solid fa-star"></i>
-                                </p>
-                                <p class="text-neutral-800 dark:text-neutral-300 mt-10 ">
-                                    <i class="fa-solid fa-list"></i>
-                                </p>
+                                <p class=" mt-5 font-semibold">Beri Rating</p>
+                                <div class="flex justify-start items-start mt-2 ">
+                                    <span class="flex flex-row-reverse">
+
+                                        <svg class="text-gray-600 cursor-pointer peer peer-hover:text-yellow-400 hover:text-yellow-400 duration-100 "
+                                            width="23" height="23" xmlns="http://www.w3.org/2000/svg"
+                                            fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z">
+                                            </path>
+                                        </svg>
+
+                                        <svg class="text-gray-600 cursor-pointer peer peer-hover:text-yellow-400 hover:text-yellow-400 duration-100 "
+                                            width="23" height="23" xmlns="http://www.w3.org/2000/svg"
+                                            fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z">
+                                            </path>
+                                        </svg>
+
+                                        <svg class="text-gray-600 cursor-pointer peer peer-hover:text-yellow-400 hover:text-yellow-400 duration-100 "
+                                            width="23" height="23" xmlns="http://www.w3.org/2000/svg"
+                                            fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z">
+                                            </path>
+                                        </svg>
+
+                                        <svg class="text-gray-600 cursor-pointer peer peer-hover:text-yellow-400 hover:text-yellow-400 duration-100 "
+                                            width="23" height="23" xmlns="http://www.w3.org/2000/svg"
+                                            fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z">
+                                            </path>
+                                        </svg>
+
+                                        <svg class="text-gray-600 cursor-pointer peer peer-hover:text-yellow-400 hover:text-yellow-400 duration-100 "
+                                            width="23" height="23" xmlns="http://www.w3.org/2000/svg"
+                                            fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z">
+                                            </path>
+                                        </svg>
+
+                                    </span>
+                                </div>
+                                <div class="grid grid-cols-3 gap-1 ">
+                                    <p class="text-neutral-800 dark:text-neutral-300 mt-10">
+                                        <i class="fa-sharp fa-solid fa-eye"></i>
+                                        {{ $novel->views_count }}
+                                    </p>
+                                    <p class="text-neutral-800 dark:text-neutral-300 mt-10 ">
+                                        <i class="fa-solid fa-star"></i>
+                                    </p>
+                                    <p class="text-neutral-800 dark:text-neutral-300 mt-10 ">
+                                        <i class="fa-solid fa-list"></i>
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
