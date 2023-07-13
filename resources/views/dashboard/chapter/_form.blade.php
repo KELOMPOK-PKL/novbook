032878fa33ee79bb03e011a87c9011db25b1e603
NdaruVictor <ndaruvictor55@gmail.com>
2023-07-13 11:26:01 +0700

    add: adjustment

--- resources/views/dashboard/chapter/_form.blade.php
+++ resources/views/dashboard/chapter/_form.blade.php
@@ -30,7 +30,7 @@
 @if (!request()->routeIs('dashboard.chapter.create'))
     <div class="mb-3">
         <x-form.label for="pdf" :value="__('Previous pdf')" class="font-semibold text-lg" />
-        <iframe src="{{ asset('storage/' . $chapter->pdf) }}" width="100px" alt="...">
+        <embed src="{{ asset('storage/' . $chapter->pdf) }}" width="100px" alt="...">
     </div>
 @endif
 @if (!request()->routeIs('dashboard.chapter.show'))
