25ff09bbdc0cd285608606f2ef8231cf721a5e5b
NdaruVictor <ndaruvictor55@gmail.com>
2023-07-14 16:16:22 +0700

    add : adjust

--- resources/views/landing/chapter/show.blade.php
+++ resources/views/landing/chapter/show.blade.php
@@ -28,6 +28,64 @@
 
         </div>
     </div>
+
+    @push('js')
+    <script>
+        var pdfUrl = "{{ asset('storage/' . $chapter->pdf) }}";
+        var myState = {
+            pdf: null,
+            currentPage: 1
+        };
+
+        pdfjsLib.getDocument(pdfUrl).promise.then(function(doc) {
+            console.log("Dokumen berisi " + doc.numPages + " halaman");
+            myState.pdf = doc;
+
+            render();
+        });
+
+        function render() {
+            var canvas = document.getElementById('my_canvas');
+            var context = canvas.getContext('2d');
+            var currentPage = myState.currentPage;
+            var pdf = myState.pdf;
+
+            pdf.getPage(currentPage).then(function(page) {
+                var viewport = page.getViewport({
+                    scale: 3
+                });
+
+                canvas.width = viewport.width;
+                canvas.height = viewport.height;
+
+                // Render halaman ke dalam canvas
+                page.render({
+                    canvasContext: context,
+                    viewport: viewport
+                });
+            });
+        }
+
+        document.getElementById('go_previous').addEventListener('click', (e) => {
+            if (myState.pdf == null || myState.currentPage === 1) {
+                return;
+            }
+            myState.currentPage -= 1;
+            render();
+        });
+
+        document.getElementById('go_next').addEventListener('click', (e) => {
+            if (myState.pdf == null || myState.currentPage >= myState.pdf.numPages) {
+                return;
+            }
+            myState.currentPage += 1;
+            render();
+            window.location.href = '#top';
+        });
+    </script>
+@endpush
+
+
    <div class="flex justify-center items-center mb-10">
     <div class="w-3/4 h-3/4 rounded-lg bg-white shadow-lg">
         <div class="flex flex-wrap items-center">
@@ -57,61 +115,6 @@
    </div>
 
 
-        @push('js')
-            <script>
-                var pdfUrl = "{{ asset('storage/' . $chapter->pdf) }}";
-                var myState = {
-                    pdf: null,
-                    currentPage: 1
-                };
-
-                pdfjsLib.getDocument(pdfUrl).promise.then(function(doc) {
-                    console.log("Dokumen berisi " + doc.numPages + " halaman");
-                    myState.pdf = doc;
-
-                    render();
-                });
-
-                function render() {
-                    var canvas = document.getElementById('my_canvas');
-                    var context = canvas.getContext('2d');
-                    var currentPage = myState.currentPage;
-                    var pdf = myState.pdf;
-
-                    pdf.getPage(currentPage).then(function(page) {
-                        var viewport = page.getViewport({
-                            scale: 3
-                        });
-
-                        canvas.width = viewport.width;
-                        canvas.height = viewport.height;
-
-                        // Render halaman ke dalam canvas
-                        page.render({
-                            canvasContext: context,
-                            viewport: viewport
-                        });
-                    });
-                }
-
-                document.getElementById('go_previous').addEventListener('click', (e) => {
-                    if (myState.pdf == null || myState.currentPage === 1) {
-                        return;
-                    }
-                    myState.currentPage -= 1;
-                    render();
-                });
-
-                document.getElementById('go_next').addEventListener('click', (e) => {
-                    if (myState.pdf == null || myState.currentPage >= myState.pdf.numPages) {
-                        return;
-                    }
-                    myState.currentPage += 1;
-                    render();
-                    window.location.href = '#top';
-                });
-            </script>
-        @endpush
 
 
 </x-landing-layout>
