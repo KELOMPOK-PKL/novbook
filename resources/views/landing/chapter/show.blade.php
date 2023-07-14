c6ee47b4c728c0cb5fc91eb691359a1324745d90
NdaruVictor <ndaruvictor55@gmail.com>
2023-07-14 08:46:28 +0700

    add : adjust

--- resources/views/landing/chapter/show.blade.php
+++ resources/views/landing/chapter/show.blade.php
@@ -1,4 +1,9 @@
 <x-landing-layout>
+    <div class="py-8 px-40">
+        <p class="font-bold text-2xl">
+            {{ $novel->title }}
+        </p>
+    </div>
     <div class="flex justify-center items-center">
         <div class="m-8 w-3/4 border-gray-300 rounded-xl bg-white shadow-lg">
             <div class="p-5">
@@ -20,14 +25,37 @@
                         id="go_next"><i class="fa-solid fa-arrow-right sm:p-3 p-1.5"></i></button>
                 </div>
             </div>
+        </div>
 
+    </div>
+    <div class="flex justify-center items-center">
+        <div class="m-8 w-3/4 flex items-center border-gray-300 rounded-lg bg-white shadow-lg">
+            <div class="w-full shrink-0 grow-0 basis-auto lg:flex lg:w-6/12 xl:w-4/12">
+                <img src="{{ asset('storage/' . $novel->image) }}" alt="..."
+                    class="container w-2/3 h-2/3 rounded-lg my-8" />
+            </div>
+            <div class="w-full shrink-0 grow-0 basis-auto lg:w-6/12 xl:w-8/12">
+                <div class="px-6 py-12 md:px-12">
+                    <h2 class="mb-6 text-xl font-bold text-center md:text-left text-black">
+                        {{ $novel->title }}
+                    </h2>
+                    <div class="ml-6">
+                        <div class="overflow-y-auto max-h-60 ">
+                            @foreach ($chapters as $chapter)
+                                <a href="{{ route('landing.chapters.show', $chapter->id) }}">
+                                    <div class="w-full border-b-2 border-gray-100 border-opacity-100 py-4">
+                                        <p>{{ $chapter->title }}</p>
+                                    </div>
+                                </a>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
-    @foreach ($chapters as $chapter)
-        {{ $chapter->title }}
-    @endforeach
-    {{ $novel->title }}
+
     @push('js')
         <script>
             var pdfUrl = "{{ asset('storage/' . $chapter->pdf) }}";
