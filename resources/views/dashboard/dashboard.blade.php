e4cf2599f65e17038f413f6034fce7a615f63dc8
NdaruVictor <ndaruvictor55@gmail.com>
2023-07-20 09:52:52 +0700

    add : adjust

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -10,4 +10,14 @@
     <div class="p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
         {{ __("You're logged in!")  }}
     </div>
+
+    <div class="flex">
+        <div class="p-6 mt-5 w-1/3 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1" >
+
+        </div>
+        <div class="p-6 mt-5 w-1/3 ml-10     overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1" >
+
+        </div>
+    </div>
+
 </x-app-layout>
