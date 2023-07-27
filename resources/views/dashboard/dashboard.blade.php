80326e37e96687cf98a404e232f7a96dbfdea1eb
NdaruVictor <ndaruvictor55@gmail.com>
2023-07-27 16:17:44 +0700

    add :adjust

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -12,30 +12,60 @@
     </div>
 
     <div class="flex">
-        
-        <div class="flex items-center justify-between font-bold text-lg p-6 mt-5 w-1/3 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1 m-2" >
-            <i class="fa-solid fa-book text-6xl"></i> 
-            <p class="text-center">
-                {{ $novelCount }}
-            </p>
+        <div class="p-6 mt-5 w-1/3 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1 m-2" >
+            <a class="flex" href="{{route('dashboard.novel.index')}}">
+                <div class="items-center text-center">
+                    <div class="bg-orange-200 rounded-md">
+                        <i class="fa-solid fa-book text-xl px-5 py-4"></i>
+                    </div>
+
+                    <p class="pt-3 text-xl">{{ $novelCount }}</p>
+                </div>
+                <div class="text-2xl">
+                    <p class="pt-4 pl-6">Novel</p>
+                </div>
+            </a>
         </div>
-        <div class="flex items-center justify-between font-bold text-lg p-6 mt-5 w-1/3 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1 m-2" >
-            <i class="fa-solid fa-bars text-6xl"></i> 
-            <p class="text-center">
-                {{ $categoryCount }}
-            </p>
+        <div class="p-6 mt-5 w-1/3 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1 m-2" >
+            <a class="flex" href="{{route('dashboard.novel.index')}}">
+                <div class="items-center text-center">
+                    <div class="bg-orange-200 rounded-md">
+                        <i class="fa-solid fa-bars text-xl px-5 py-4"></i>
+                    </div>
+
+                    <p class="pt-3 text-xl">{{ $categoryCount }}</p>
+                </div>
+                <div class="text-2xl">
+                    <p class="pt-4 pl-6">Kategori</p>
+                </div>
+            </a>
         </div>
-        <div class="flex items-center justify-between font-bold text-lg p-6 mt-5 w-1/3 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1 m-2" >
-            <i class="fa-solid fa-user text-6xl"></i> 
-            <p class="text-center">
-                {{ $userCount }}
-            </p>
+        <div class="p-6 mt-5 w-1/3 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1 m-2" >
+            <a class="flex">
+                <div class="items-center text-center">
+                    <div class="bg-orange-200 rounded-md">
+                        <i class="fa-solid fa-user text-xl px-5 py-4"></i>
+                    </div>
+
+                    <p class="pt-3 text-xl">{{ $novelCount }}</p>
+                </div>
+                <div class="text-2xl">
+                    <p class="pt-4 pl-6">Pengguna</p>
+                </div>
+            </a>
         </div>
-        <div class="flex items-center justify-between font-bold text-lg p-6 mt-5 w-1/3 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1 m-2" >
-            <i class="fa-solid fa-list text-6xl"></i> 
-            <p class="text-center">
-                {{ $chapterCount }}
-            </p>
+        <div class="p-6 mt-5 w-1/3 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1 m-2" >
+            <a class="flex" href="{{route('dashboard.novel.index')}}">
+                <div class="items-center text-center">
+                    <div class="bg-orange-200 rounded-md">
+                        <i class="fa-solid fa-list text-xl px-5 py-4"></i>
+                    </div>
+                    <p class="pt-3 text-xl">{{ $chapterCount }}</p>
+                </div>
+                <div class="text-2xl">
+                    <p class="pt-4 pl-6">Chapter</p>
+                </div>
+            </a>
         </div>
     </div>
 
